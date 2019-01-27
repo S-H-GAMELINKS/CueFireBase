8aec322f62b7ea05f0c31a7d9e3d01b627961613
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-27 21:07:33 +0900

    add github link

--- assets/components/web/Contact.vue
+++ assets/components/web/Contact.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
         <h1>Contact Pages</h1>
+        <p>
+            GitHub:<a href="https://github.com/S-H-GAMELINKS/CueFireBase">CueFireBase</a>
+        </p>
     </div>    
 </template>
