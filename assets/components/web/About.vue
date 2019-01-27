06ef7447429fb322e73482d3000e8330399bfcfc
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-27 21:10:37 +0900

    add web/About.vue content

--- assets/components/web/About.vue
+++ assets/components/web/About.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
         <h1>About Pages</h1>
+        <p>
+            This is Chat Application, using Cue(C++/Vue.js Web Frame Work) and FireBase
+        </p>
     </div>    
 </template>
